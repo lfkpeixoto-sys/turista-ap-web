353d902113ba5b0f087d5a1c2f4cd33f69086da8
Luis Felipe <lfkpeixoto@gmail.com>
2025-11-12 21:20:56 -0300

    Projeto inicial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>🌎 TuristaAp Web</h1>
+      <p>Base configurada com sucesso!</p>
+    </div>
+  )
+}
+
+export default App
